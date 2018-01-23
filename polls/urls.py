f447d859209dfb4cd9ab83b431137c04342a3f3e
Demid Afonichev <demidafonichev@gmail.com>
2018-01-23 22:15:41 +0300

    Create API method for submitting poll answers. Need to modify (see TODO).

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import get_polls
+from .views import get_polls, submit_answers
 
 urlpatterns = [
     url(r'^get_polls/', get_polls, name='get_polls'),
+    url(r'^submit_answers/', submit_answers, name='get_answers'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,10 +2,13 @@ import json
 
 from django.http import HttpResponse
 from rest_framework import status
+from rest_framework.decorators import api_view
 
-from .models import Poll, Question
+from accounts.models import Student
+from .models import Poll, Question, ScaleAnswer, TextAnswer
 
 
+@api_view(['GET'])
 def get_polls(request):
     polls = []
     for poll in Poll.objects.all():
@@ -18,3 +21,22 @@ def get_polls(request):
     return HttpResponse(status=status.HTTP_200_OK,
                         content=json.dumps({'polls': polls}),
                         content_type='application/json')
+
+
+@api_view(['POST'])
+def submit_answers(request):
+    # TODO: change student id by taking it from request.user
+    student = Student.objects.get(pk=3)
+    answers = json.loads(request.body)['answers']
+    for answer in answers:
+        question = Question.objects.get(pk=answer['question'])
+        if answer['scale']:
+            ScaleAnswer.objects.create(student=student,
+                                       question=question,
+                                       answer=answer['answer'])
+        else:
+            TextAnswer.objects.create(student=student,
+                                      question=question,
+                                      answer=answer['answer'])
+
+    return HttpResponse(status.HTTP_200_OK)
